3cc8d8b15fcd6f4ccd846317efe6a8bed2e1f709
Airplanesaviation2361 <karan.kuppa@icloud.com>
2026-04-06 13:38:30 +0400

    Update airline list and fetch URL in init function

--- va.js
+++ va.js
@@ -1,11 +1,11 @@
 function init(){
     vadiv = document.createElement("div");
     vadiv.id = "vadiv"
-    vadiv.innerHTML = '<h5>Your Virtual Airlines<button class="mdl-button mdl-js-button mdl-button--raised mdl-button--colored" onclick="addAirline()" style="float: right; color: white;">+ Add airline</button></h5><ul id="valist" class=" geofs-list geofs-visible"></ul>'
+    vadiv.innerHTML = '<h5>Your Virtual Airlines<button class="mdl-button mdl-js-button mdl-button--raised mdl-button--colored" onclick="addAirline()" style="float: right; color: white;">+ Add airline</button></h5><ul id="valist" class=" Betageofs-list Betageofs-visible"></ul>'
     document.getElementById("liverylist").parentNode.appendChild(vadiv)
     links = localStorage.links.split(",");
     links.forEach(async function(e){
-        await fetch("https://raw.githubusercontent.com/kolos26/GEOFS-LiverySelector/main/livery.json").then(res => res.json()).then(data => valiveryobj[e] = data)
+        await fetch("https://raw.githubusercontent.com/AirplanesAviation2361/BetaGEOFS-LiverySelector/main/livery.json").then(res => res.json()).then(data => valiveryobj[e] = data)
     });
 }
 
